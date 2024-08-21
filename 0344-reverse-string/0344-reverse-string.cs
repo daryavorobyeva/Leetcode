d2ac9b0324116ca336bd8b1012ad4c66011bf993
Darya Vorobyeva <161718065+daryavorobyeva@users.noreply.github.com>
2024-08-21 21:07:22 +0300

    Time: 224 ms (60.71%), Space: 129.4 MB (44.42%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left <= right)
+        {
+            char tmp = s[right];
+            s[right] = s[left];
+            s[left] = tmp;
+
+            left++;
+            right--;
+        }
+    }
+}
